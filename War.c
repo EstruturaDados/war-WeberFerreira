378a015e49a16853df1c96477b6e3b3391aa1b0e
WeberFerreira <negociowf@gmail.com>
2025-11-06 00:07:09 +0000

    Organizei o codigo, adicionando laco for, entradas de dados e exibicao dos dados.

--- War.c
+++ War.c
@@ -8,5 +8,38 @@ typedef struct
     int tropa;
 } Territorio;
 
-Territorio territorios[5];
 
+int main () {
+    
+    Territorio territorios[5]; //capacidade para 5
+    int i; //contador para o laco for
+    //utilizando for como orientado na atividade
+    printf("CADASTRO DE TERRITORIOS\n");
+    //entrada de dados no laco
+    for(i=0;i<5;i++){
+        printf("\nTerritório %d\n", i+1);
+
+        printf("Digite o nome: ");    
+        scanf("%s", territorios[i].nome);
+
+        printf("Qual é a cor: ");
+        scanf("%s", territorios[i].cor);
+
+        printf("Número de tropas: ");
+        scanf("%d", &territorios[i].tropa);
+
+    }
+    
+     //exibicao dos dados
+    printf("\nTERRITORIOS CADASTRADOS\n");
+    
+    for (i=0; i<5; i++){
+    printf("\n Território %d\n ", i + 1);
+    printf("Nome: %s\n", territorios[i].nome);
+    printf("Cor: %s\n", territorios[i].cor);
+    printf("Número de tropas: %d\n", territorios[i].tropa);
+    }
+    
+    return 0;
+
+}
